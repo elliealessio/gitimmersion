5fb83a44af51e46121b6e540b52ad393122b0125
Ellie <ealessio82@gmail.com>
2017-11-02 16:02:11 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,4 @@
-# Default is "World"
-# Author: Ellie Alessio (ealessio@uncc.edu)
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
